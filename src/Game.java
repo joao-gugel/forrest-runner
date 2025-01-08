b40057dbc139343be7f8b0da18e6ab5cc833f6e4
joao gugel <ojoaogugel@gmail.com>
2025-01-08 17:05:42 -0300

    feat: add game main thread

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,63 @@
 public class Game implements Runnable {
     private Window window;
+    private Thread gameThread;
     private GamePanel gamePanel;
 
     public Game() {
         this.gamePanel = new GamePanel();
         this.window = new Window(this.gamePanel);
+
+        this.startGame();
+    }
+
+    private void startGame() {
+        this.gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    private void update() {
+        this.gamePanel.repaint();
     }
 
     @Override
     public void run() {
+        double timePerFrame = 1_000_000_000.0 / Settings.FPS_SET;
+        double timePerUpdate = 1_000_000_000.0 / Settings.UPS_SET;
 
+        long prevTime = System.nanoTime();
+        int FPS = 0;
+        int UPDATES = 0;
+
+        long lastCheck = System.currentTimeMillis();
+        double deltaU = 0;
+        double deltaFrames = 0;
+
+        while (true) {
+            long currTime = System.nanoTime();
+
+            deltaU += (currTime - prevTime) / timePerUpdate;
+            deltaFrames += (currTime - prevTime) / timePerFrame;
+            prevTime = currTime;
+
+            if (deltaU >= 1) {
+                update();
+                UPDATES++;
+                deltaU--;
+            }
+
+            if (deltaFrames >= 1) {
+                gamePanel.repaint();
+                deltaFrames--;
+                FPS++;
+            }
+
+            if (System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + FPS + " | UPS: " + UPDATES);
+                FPS = 0;
+                UPDATES = 0;
+            }
+        }
     }
+
 }
--- src/Settings.java
+++ src/Settings.java
@@ -1,4 +1,7 @@
 public class Settings {
     public static final int SCREEN_WIDTH = 800;
     public static final int SCREEN_HEIGHT = 600;
+
+    public static final int FPS_SET = 60;
+    public static final int UPS_SET = 120;
 }
